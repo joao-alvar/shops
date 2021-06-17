a3c764fc9746652dbc4313c76927aef152ef8e43
João Alvarenga <jvalvarenga@mailfence.com>
2021-06-17 19:20:17 -0300

    add new files

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -18,8 +18,8 @@ const Nav = props => {
             <div className="nav__main__links">
             <div className="logo"><h2>Shops.</h2></div>
             <ul className="nav__primary__ul">
-            <li className="li__shop__for__mulher"><h3><a href="#">Mulher</a></h3></li>
-            <li className="li__shop__for__homem"><h3><a href="#">Homem</a></h3></li>
+            <li className="li__shop__for__mulher"><h3><a className="a__shop__for__mulher" href="#">Mulher</a></h3></li>
+            <li className="li__shop__for__homem"><h3><a className="a__shop__for__homem" href="#">Homem</a></h3></li>
             </ul>
             </div>
             <div className="nav__icons">
@@ -32,9 +32,9 @@ const Nav = props => {
            <div className="headerBanner">
            <div className="banner__shop__for__mulher"><a href="#">Mulher</a></div>
            <div className="banner__shop__text"><strong>
-           SALE: AGORA GANHE ATÉ 80% DE DESCONTO!
+           PROMOÇÂO: AGORA GANHE ATÉ 80% DE DESCONTO!
            <br/>
-           A SALE DO SHOPS. ficou MAIOR
+          &nbsp;A PROMOÇÂO DO SHOPS. ficou Ainda MAIOR
            </strong></div>
            <div className="banner__shop__for__homem"><a href="#">Homem</a></div>
            </div>
