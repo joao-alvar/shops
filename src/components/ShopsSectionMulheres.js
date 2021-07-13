35305710737310cb2a5ab3619d6f8ef5e1d49d93
João Alvarenga <jvalvarenga@mailfence.com>
2021-07-12 23:55:28 -0300

    add updates

--- src/components/ShopsSectionMulheres.js
+++ src/components/ShopsSectionMulheres.js
@@ -1,19 +1,23 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import shopsMulheres from '../images/shopsMulheres.jpg';
 
 const ShopsSectionMulheres = props => {
     return (
+        <>
         <div className="ShopsContainer" style={{
             backgroundImage: `url(${shopsMulheres}`
         }}>
             <div className="Shops__page__container content__md">
             <span className="shops__glass">
-            <div  className="shops__banner"><h3 className="banner__content">Os melhores estilos</h3></div>
-            <div  className="shops__banner__btn"><Link className="banner__link"><h3 className="banner__btn">compre agora</h3></Link></div>
+            <div  className="shops__banner banner__md__section"><h3 className="banner__content">Marcas e tendências</h3></div>
+            <div  className="shops__banner__btn"><a href="#products" className="banner__link"><h3 className="banner__btn">compre agora</h3></a></div>
             </span>
             </div>
         </div>
+        <section id="products">
+
+        </section>
+        </>
     );
 };
 
