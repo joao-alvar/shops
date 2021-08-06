a246402da42fa491c2b221a98c1ff218e9b9e83b
João Alvarenga <jvalvarenga@mailfence.com>
2021-08-06 01:05:51 -0300

    add updates

--- src/components/NavSection.js
+++ src/components/NavSection.js
@@ -1,88 +1,146 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
-import PersonOutlineIcon from '@material-ui/icons/PersonOutline';
-import LocalMallOutlinedIcon from '@material-ui/icons/LocalMallOutlined';
-import SearchIcon from '@material-ui/icons/Search';
-import { auth } from '../firebase/utils';
+import React from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import PersonOutlineIcon from "@material-ui/icons/PersonOutline";
+import LocalMallOutlinedIcon from "@material-ui/icons/LocalMallOutlined";
+import SearchIcon from "@material-ui/icons/Search";
+import { auth } from "../firebase/utils";
 
-const NavSection = props => {
-    const { currentUser } = props;
-    return (
-        <header>
-        <div className="headerContainer">
-           <div className="headerRoom">
-           <div className="headerRoom__wrap">
-           <ul className="headerRoom__ul">
-           <li><Link to="/">Marketplace</Link></li>
-           <li><Link to="/" className="faq">Ajuda &#x26; FAQs</Link></li>
-           </ul>
-           </div>
-           </div>
-            <nav>
-            <div className="nav__main__links">
-            <div className="logo"><h2><Link to="/">Shops.</Link></h2></div>
+const NavSection = (props) => {
+  const { currentUser } = props;
+  return (
+    <header>
+      <div className="headerContainer">
+        <div className="headerRoom">
+          <div className="headerRoom__wrap">
+            <ul className="headerRoom__ul">
+              <li>
+                <Link to="/">Marketplace</Link>
+              </li>
+              <li>
+                <Link to="/" className="faq">
+                  Ajuda &#x26; FAQs
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <nav>
+          <div className="nav__main__links">
+            <div className="logo">
+              <h2>
+                <Link to="/">Shops.</Link>
+              </h2>
+            </div>
             <ul className="nav__primary__ul">
-            <li className="li__shop__for__mulher"><h3><Link className="a__shop__for__mulher active__section" to="/shopsmulheres">Mulher</Link></h3></li>
-            <li className="li__shop__for__homem"><h3><Link className="a__shop__for__homem" to="/shopshomens">Homem</Link></h3></li>
+              <li className="li__shop__for__mulher">
+                <h3>
+                  <Link
+                    className="a__shop__for__mulher active__section"
+                    to="/shopsmulheres"
+                  >
+                    Mulher
+                  </Link>
+                </h3>
+              </li>
+              <li className="li__shop__for__homem">
+                <h3>
+                  <Link className="a__shop__for__homem" to="/shopshomens">
+                    Homem
+                  </Link>
+                </h3>
+              </li>
             </ul>
-            </div>
-            <div className="search__bar__container">
+          </div>
+          <div className="search__bar__container">
             <form className="searchForm">
-            <input type="search" placeholder="pesquisar por produtos" className="search__bar" required></input>
-            <button type="submit" className="search__bar__btn"><SearchIcon  style={{ fontSize: 20, cursor: "pointer" }} className="search__bar__btn__icon"  /></button>
+              <input
+                type="search"
+                placeholder="pesquisar por produtos"
+                className="search__bar"
+                required
+              ></input>
+              <button type="submit" className="search__bar__btn">
+                <SearchIcon
+                  style={{ fontSize: 20, cursor: "pointer" }}
+                  className="search__bar__btn__icon"
+                />
+              </button>
             </form>
-            </div>
-            <div className="nav__icons">
+          </div>
+          <div className="nav__icons">
             <ul className="icon__list">
-            <div className="tooltip__container">
-            <li><PersonOutlineIcon  style={{ fontSize: 29, cursor: "pointer" }} /></li>
-            <span className="tooltip__links">
-            {!currentUser && (
-                <ul>
-                    <li><h3>minha conta</h3></li>
-                    <li><Link to="/signin" className="sign__in__btn">entrar</Link></li>
-                    <li><Link to="/registration" className="register__btn">resgistre-se</Link></li>
-                </ul>
-                    )}
+              <div className="tooltip__container">
+                <li>
+                  <PersonOutlineIcon
+                    style={{ fontSize: 29, cursor: "pointer" }}
+                  />
+                </li>
+                <span className="tooltip__links">
+                  {!currentUser && (
+                    <ul>
+                      <li>
+                        <h3>minha conta</h3>
+                      </li>
+                      <li>
+                        <Link to="/signin" className="sign__in__btn">
+                          entrar
+                        </Link>
+                      </li>
+                      <li>
+                        <Link to="/registration" className="register__btn">
+                          resgistre-se
+                        </Link>
+                      </li>
+                    </ul>
+                  )}
 
-            {currentUser && (
-                 <ul className="loged__ul">
-                  <li><h3>minha conta</h3></li>
-                  <li><Link className="sign__in__btn">meus pedidos</Link></li>
-                  <li onClick={() =>  auth.signOut()}><Link className="register__btn">sair</Link></li>
-                  </ul>
-                   )}
-
-            </span></div>
-            <li><LocalMallOutlinedIcon  style={{ fontSize: 27, cursor: "pointer"  }} /></li>
+                  {currentUser && (
+                    <ul className="loged__ul">
+                      <li>
+                        <h3>minha conta</h3>
+                      </li>
+                      <li>
+                        <Link className="sign__in__btn">meus pedidos</Link>
+                      </li>
+                      <li onClick={() => auth.signOut()}>
+                        <Link className="register__btn">sair</Link>
+                      </li>
+                    </ul>
+                  )}
+                </span>
+              </div>
+              <li>
+                <LocalMallOutlinedIcon
+                  style={{ fontSize: 27, cursor: "pointer" }}
+                />
+              </li>
             </ul>
-            </div>
-            </nav>
-           <div className="headerBannerShops">
-           <div className="shops__banner__text"><strong>
-           PROMOÇÂO: 20% DE DESCONTO
-           <br/>
-          &nbsp;na compra. de qualquer produto!
-           </strong>
-           </div>
-           <div className="shops__banner__content"><strong>
-           entrega gratis para todo o país
-           </strong>
-           
-           </div>
-           </div>
-           </div>
-        </header>
-    );
+          </div>
+        </nav>
+        <div className="headerBannerShops">
+          <div className="shops__banner__text">
+            <strong>
+              PROMOÇÂO: 20% DE DESCONTO
+              <br />
+              &nbsp;na compra. de qualquer produto!
+            </strong>
+          </div>
+          <div className="shops__banner__content">
+            <strong>entrega gratis para todo o país</strong>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 NavSection.defaultProps = {
-    currentUser: null
+  currentUser: null,
 };
 
 const mapStateToprops = ({ user }) => ({
-    currentUser: user.currentUser
-})
+  currentUser: user.currentUser,
+});
 
 export default connect(mapStateToprops, null)(NavSection);
